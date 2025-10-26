d93c5083f93efccadcbc0ec5688770be1246b983
Pablo Basso <pablobasso25@gmail.com>
2025-10-26 16:20:47 -0300

    add ListaUsuario.jsx component

---
+++ src/components/ListaUsuario.jsx
@@ -0,0 +1,10 @@
+// Importamos React - necesario para usar JSX
+import React from "react";
+
+// Importamos componentes de React Bootstrap
+// Row, Col: sistema de grid para organizar las tarjetas
+// Alert: para mostrar mensaje cuando no hay usuarios
+import { Row, Col, Alert } from "react-bootstrap";
+
+// Importamos el componente UserCard que mostraremos múltiples veces
+import CardUsuario from "./CardUsuario";
